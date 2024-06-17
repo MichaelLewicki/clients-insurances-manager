430b9c00294e607930f8a068fc5e0a2200b60a85
Michael Lewicki <lewickiclass@gmail.com>
2024-06-17 16:36:43 -0400

    code order was modified

--- src/test/java/cl/lewickidev/MainTest.java
+++ src/test/java/cl/lewickidev/MainTest.java
@@ -12,12 +12,12 @@ public class MainTest {
 
     @Test
     void testInsuranceClientsByRUT() {
-        Map<String, List<String>> result = Main.insuranceClientsByRUT();
-
         Map<String, List<String>> expectedData = new HashMap<>();
         expectedData.put("SEGURO DE VIDA", List.of("94020190", "86620855", "73826497", "7317855K"));
         expectedData.put("SEGURO APV", List.of("94020190", "99804238", "73826497", "7317855K"));
         expectedData.put("SEGURO COMPLEMENTARIO DE SALUD", List.of("94020190", "86620855", "73826497", "88587715", "7317855K"));
+
+        Map<String, List<String>> result = Main.insuranceClientsByRUT();
 
         assertEquals(expectedData.keySet(), result.keySet());
         assertEquals(expectedData.values().toString(), result.values().toString());
@@ -37,9 +37,9 @@ public class MainTest {
 
     @Test
     void testInsuranceSortedByHighestBalance() {
-        List<Integer> insuranceIds = Main.insuranceSortedByHighestBalance();
-
         List<Integer> expectedOrder = List.of(2, 1, 3);
+
+        List<Integer> insuranceIds = Main.insuranceSortedByHighestBalance();
 
         assertEquals(expectedOrder, insuranceIds, "Los IDs no están en el orden esperado");
     }
